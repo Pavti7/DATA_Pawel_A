e19af961a92244215a3a8d563c56cb45854f0b97
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-12 16:09:52 +0100

    zadanie i rozwiązanie na koniec zajęć 12.03.2023

---
+++ Zadanie End.py
@@ -0,0 +1,32 @@
+# 1. Napisz funkcję is_even, która przyjmuje jeden argument
+#     A następnie zwraca True - jeżeli liczba jest parzysta,
+#     w przeciwnym razie False
+#
+#     Wykorzystaj nową funkcję do poprzedniego zadania
+#     (tam gdzie użytkownik podawał liczbę, a następnie otrzymywał komunikat
+#      liczba parzysta/nieparzysta)
+
+# 2. Napisz funkcję my_pow, która pozwoli na zwrócenie potęgi wybranej liczby
+#     do wybranej potęgi. Liczbe oraz potegę podajemy jako argumenty do tej fukncji
+# 1
+liczba = int(input("Podaj liczbę parzystą: "))
+if liczba % 2 == 0:
+    print("True")
+else:
+     print("False")
+# 1 (alternatywna wersja Piotra)
+def is_even(x):
+    return x % 2 == 0
+
+y = 11
+if is_even(y):
+    print("Parzysta")
+else:
+    print("Nieparzyste")
+
+# 2
+def my_pow(base, power):
+    return base ** power
+
+z = my_pow(2, 10)
+print(z)
