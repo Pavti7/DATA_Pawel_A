d56fc572b97eb93ae706fd250f825a557ed383e6
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-12 13:25:28 +0100

    konwersja danych

---
+++ Podstawy_zadania.py
@@ -0,0 +1,21 @@
+# 1. Utwórz zmienne: x i y oraz przypisz do nich dowolne liczby.
+#    Następnie wyświetl wszystkie operacje arytmetczne z wykorzystaniem zmiennych
+
+x = 10
+y = 2
+
+print("Dodawanie:", x + y)
+print("Odejmowanie:", x - y)
+print(x / y)
+print(x * y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+# 2. Utwórz zmienną tekstową (dowolny) tekst, a następnie dopisz do niego (do zmiennej)
+#    Tekst: "a kot ma pierdolca"
+
+txt = "Ala ma kota, "
+print(txt + "a kota ma pierdolca.")
+txt += "a kot ma pierdolca"
+print(txt)
