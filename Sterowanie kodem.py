b31397c206b776957310eb71591457003a9a42c2
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-12 15:19:21 +0100

    określanie warunków krytycznych, brzegowych. używanie (if) - zadania i rozwiązania

--- Sterowanie kodem.py
+++ Sterowanie kodem.py
@@ -33,3 +33,29 @@ elif product == "zgniła cebula" and cost <= 1.9:
     print("Nie kup[uj tego gówna")
 else:
     print("Wychodzisz bez zakupów.")
+
+# Zadanie z if
+#1. Użytkownik podaje liczbę, a nastęnie dostaje odpowiedź czy podana liczba
+#    jest parzysta/nieparzysta.
+liczba = int(input("Podaj liczbę: "))
+if liczba % 2 == 0:
+    print("liczba parzysta ")
+else:
+    print("Liczba niepatrzysta.")
+
+# 2. Użytkownik podaje liczbę, a nastęnie program:
+#   Wyświetli "Pif Paf", jeżeli podana liczba jest podzielna przez 3 i 5
+#   Wyświetli "Pif", jeżeli podana liczba jest podzielna tylko przez 3
+#   Wyświetli "Paf", jeżeli podana liczba jest podzielna tylko przez 5
+#   Wyświetli komunikat "Twoja liczba to: " + podana liczba, jeżeli nie spełniono
+#   żadnego z powyższych warunków.
+Liczba1 = int(input("Podaj liczbę: "))
+if Liczba1 % 5 == 0 and Liczba1 % 3 == 0:
+    print("Pif Paf")
+elif Liczba1 % 3 == 0:
+    print(Pif)
+elif Liczba1 % 5 == 0:
+    print(Paf)
+else:
+    print("Twoja liczba to: " + str(Liczba1))
+
