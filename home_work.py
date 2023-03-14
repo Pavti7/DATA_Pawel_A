c42bedd140e380bdcf3b1945b009b0c82dae5558
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-14 13:22:03 +0100

    ćwiczenia w trakcie czytania książki Python dla każdego

---
+++ home_work.py
@@ -0,0 +1,15 @@
+print(" ")
+print("Jaki jest Mój ulubiony smak lodów")
+input("\1\1Aby się tego dowiedzieć, naciśnij klawisz Enter.")
+print("waniliowe")
+input("\2\2Aby dowiedzieć się jaki jeszcze smak, ponownie naciśnij Enter.")
+print("malaga :)")
+input("\3\3Czy jeszcze jakiś smak? ponownie naciśnij klawisz Enter.")
+print("czekoladowe :)")
+input("\4\4Zakończ program wciskając ENTER!")
+
+# 2
+#
+input("\n\nNaciśnij klawisz Enter, aby poznać moje imię:")
+print("Paweł")
+input("\1\1Zakończ program klawiszem <<ENTER>>")
