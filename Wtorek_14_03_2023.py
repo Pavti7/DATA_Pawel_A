9c9dfd2fa16eaf60623b04546c88d21a8fe4c194
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-14 21:12:23 +0100

    brudnopis...

---
+++ Wtorek_14_03_2023.py
@@ -0,0 +1,27 @@
+# Program przyjmuje od użytkownika dwie liczby:
+#     1. Liczba prawidłowych odpowiedzi (int)
+#     2. Liczba pytań (int)
+# Następnie program wyświetla odpowiedni komunikat na konsoli zależnie od % prawidłowych odpowiedzi:
+#     100% - 90% : 5.0, 89% - 76% : 4.5, 75% - 70% : 4.0
+#     69% - 60% : 3.5, 59% - 50% : 3.0, 49% - 0% : 2.0
+
+# Napisz funkcję o nazwie show_temp_status, która przyjmuje jeden argument typu float
+# Następnie zwraca str (nie wykonuje print!) zależnie od wartości podanego argumentu:
+#     Poniżej 36.4 - wychłodzenie
+#     <36.4 36.8> - norma
+#     <36.9, 37.0> - stan podgorączkowy
+#     Powyżej 3.71 - gorączka
+
+total = int(input("podaj ilość pytań: "))
+correct = int(input("Podaj ilość prawidłowych odp: "))
+
+result_procent = int((corect / total) *100)
+
+if 
+
+num_1 = int(input("Podaj liczbę odpowiedzi: "))
+if 90 <= num_1 <= 100:
+    print("5.0")
+    elif 76 <= num_1 <=89
+    print("4.5")
+
