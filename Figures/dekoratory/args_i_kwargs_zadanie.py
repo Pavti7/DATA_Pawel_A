d199638ad438141a03f31ca29469cf83303416d9
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-19 14:58:03 +0100

    *args i *kwargs - zadanie

---
+++ Figures/dekoratory/args_i_kwargs_zadanie.py
@@ -0,0 +1,49 @@
+# Napisz funkcję, która jako PIERWSZY argument przyjmuje rodzaj operacji
+#     "suma", "różnica", "iloczyn"
+# a następnie wykona sumowanie/odejmowanie/mnożenie wszystkich argumentów
+# podanych po pierwszym. Ilość argumentów do obliczeń może być dowolna
+
+def calculation(option: str, *args) -> float:
+    result = args[0]
+    if len(args) >= 2:
+        if option == "suma":
+            for i in range(1, len(args)):
+                result += args[i]
+        elif option == "różnica":
+            for i in range(1, len(args)):
+                result -= args[i]
+        elif option == "iloczyn":
+            for i in range(1, len(args)):
+                result *= args[i]
+
+        return result
+    else:
+        return args[0]
+
+print(calculation("suma", 1, 2, 3))
+print(calculation("rożnica", 23, 45, 5))
+print(calculation("iloczyn", 2, 2))
+
+# ponizej moje nieudolne próby poraczenia sobiew z zadaniem :(
+# def summary(*args) -> int:
+#     print(f"Elements: {args}")
+#     result = 0
+#     for i in args:
+#         result += i
+#
+#     return result
+# def minus(*args) -> int:
+#     print(f"Wynik: {args}")
+#     result = 0
+#     for i in args:
+#         result -= i
+# def iloczyn(*args) -> int:
+#     print(f"Wynik: {args}")
+#     print(f"Wynik: {args}")
+#     result = 0
+#     for i in args:
+#         result *= i
+#
+# def operacje(prefix: str, *args) -> None:
+#     for i in args:
+#         print(f"{prefix} -> {i}")
