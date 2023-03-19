350b385b7701685f8de17103b19557514d9aab7a
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-19 10:00:38 +0100

    zadanie splitter danych

---
+++ programowanie_obiektowe/splitter.py
@@ -0,0 +1,18 @@
+# start - godzina rozpoczęcia seansu
+# playtime - ile sekund oglądaliśmy
+
+p = 3.5
+s = 5.6
+
+5: 0.4 (do 6)
+6: 1.0 (do 7)
+7: 1.0 (do 8)
+8: 1.0 (do 9)
+9: 0.1 (do 10) <-- p + s
+
+0: 0.0 - 1.00
+1: 1.01 - 2.00
+2: 2.01 - 3.00
+
+...
+10: 10.01 - 11.00
