4b580c740ecbd2f2dd49e8f1c9e0306aeb8f6ad1
Pavti7 <127602345+Pavti7@users.noreply.github.com>
2023-03-19 16:18:20 +0100

    Update args_i_kwargs_zadanie.py
    dodano uzupełnienie o rozwiązania kolejnych zadań: iloraz, zabezpieczenie przed /0 i pomijanie błędów w wyniku działania kodu

--- Figures/dekoratory/args_i_kwargs_zadanie.py
+++ Figures/dekoratory/args_i_kwargs_zadanie.py
@@ -2,6 +2,9 @@
 #     "suma", "różnica", "iloczyn"
 # a następnie wykona sumowanie/odejmowanie/mnożenie wszystkich argumentów
 # podanych po pierwszym. Ilość argumentów do obliczeń może być dowolna
+# Rozwiń funkcjonalność zadania z (opcja, *args) o nową opcję "iloraz".
+# Zabezpiecz program, że w przypadku dzielenia przez zero kontynuuj działanie
+# iloraz (dzielenia kolejnych wartości), pomijając błędny (umieść continue w except)
 
 def calculation(option: str, *args) -> float:
     result = args[0]
@@ -15,7 +18,13 @@ def calculation(option: str, *args) -> float:
         elif option == "iloczyn":
             for i in range(1, len(args)):
                 result *= args[i]
-
+        elif option == "iloraz":
+            for i in range(1, len(args)):
+                try:
+                    result /= args[i]
+                except ZeroDivisionError:
+                    continue
+                    
         return result
     else:
         return args[0]
@@ -23,6 +32,7 @@ def calculation(option: str, *args) -> float:
 print(calculation("suma", 1, 2, 3))
 print(calculation("rożnica", 23, 45, 5))
 print(calculation("iloczyn", 2, 2))
+print(calculation("różnica", -2))
 
 # ponizej moje nieudolne próby poraczenia sobiew z zadaniem :(
 # def summary(*args) -> int:
