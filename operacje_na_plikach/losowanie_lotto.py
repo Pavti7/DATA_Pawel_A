0d35cc7f59505714c6de2935cd6fbf1c63960b21
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-03-21 20:29:06 +0100

    zadanie z zapisywania w pliku LOTTO

---
+++ operacje_na_plikach/losowanie_lotto.py
@@ -0,0 +1,22 @@
+# Użytkownik podaje ilość losowań (n), a następnie program zapisuje
+# do pliku n przykładowych wyników lotto
+# * Pomijaj duplikaty w losowaniu
+#
+# Przykład: użytkownik podaje n = 2
+# Wynik (zawartość pliku):
+#
+# 2 6 12 40 31 22
+# 12 34 49 21 1 12
+from random import randint
+print(randint(1, 49))
+
+n = int(input("Podaj ilość losowań: "))
+
+for l in range(n):
+    numbers = set()
+    while len(numbers) < 6:
+        numbers.add(f"{randint(1, 49)} ")
+
+    with open("lotto.txt", "a+", encoding="utf-8") as file:
+        file.writelines(numbers)
+        file.write("\n")
