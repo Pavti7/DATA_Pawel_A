29878b42d430a943c14fe225fcacc1a2aa0d1aa5
Pawel Awtuch <pawel.awtuch@gmail.com>
2023-05-17 19:33:47 +0200

    projekcik z YT

--- Learn_YT/SayHello.py
+++ Learn_YT/SayHello.py
@@ -4,6 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+
 class SayHello(App):
     def build(self):
         self.window = GridLayout()
@@ -11,5 +12,5 @@ class SayHello(App):
 
         return self.window
 
-if __name__== "__main":
+if __name__ == "__main__":
     SayHello().run()
